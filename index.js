1378c3813f0de025faa133d48322d339ab8df525
jay kumar <jay94588@gmail.com>
2025-09-30 16:14:08 +0530

    deploy

--- index.js
+++ index.js
@@ -8,33 +8,35 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// ✅ Allow both local & deployed frontend
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: [
+    "http://localhost:5173",
+    "https://crp-frontend-mu.vercel.app",
+  ],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
-
-// api's
+// routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`✅ Server running at port ${PORT}`);
+});
